063174a48fc290c02bde6c46752a69b5c5ef46d0
도훈 <modooboiroo@gmail.com>
2025-04-14 17:23:49 +0900

    first commit

---
+++ src/main/kotlin/com/example/demoresilience4j/DemoResilience4jApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demoresilience4j
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoResilience4jApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoResilience4jApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/demoresilience4j/DemoResilience4jApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demoresilience4j
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoResilience4jApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
